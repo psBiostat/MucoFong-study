3fdc3de49c849823d4bba2914e22c8450202929f
psBiostat <perrine.soret@gmail.com>
2018-11-10 11:47:00 +0100

    Update Principal.R

--- Principal.R
+++ Principal.R
@@ -1,4 +1,14 @@
-source("Phy
+###################################################################################
+#
+#      Stability OTU selection for microbiome data
+#
+###################################################################################
+
+# Data
+# list of four objects:
+#     $X : matrix of relative abondance
+#     $
+source("Function.R")
 load("Simu_Data.RData")
 
 Bootstrap_Estimation <- Boots_Phyglm_LOO_CV_Cont(Simu_Data, 200)
