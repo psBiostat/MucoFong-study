79bdf13244c94e39972c579d5491ea1c9d80989a
psBiostat <perrine.soret@gmail.com>
2018-11-10 11:51:53 +0100

    Update Principal.R

--- Principal.R
+++ Principal.R
@@ -7,7 +7,8 @@
 # Data
 # list of four objects:
 #     $X : matrix of relative abondance
-#     $
+#     $Y : numeric vector response variable
+#     $Taxonomy : 
 source("Function.R")
 load("Simu_Data.RData")
 
